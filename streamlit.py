5ee4b0bbfa0db859cefbba6c4116a0c533de0dd4
Rishab Kumar <rishabkumar7@gmail.com>
2024-04-24 08:36:38 -0700

    name change and fixed list

--- streamlit.py
+++ streamlit.py
@@ -2,12 +2,12 @@ import streamlit as st
 import app as ai
 import textwrap
 
-st.title("☁️Cloud Homework")
+st.title("☁️Cloud Project Generator")
 
 
 certification = st.selectbox(
     'Microsoft Azure Certification',
-    ('AZ-104', 'AZ-204' 'AZ-305', 'AZ-400'))
+    ('AZ-104', 'AZ-204', 'AZ-305', 'AZ-400'))
 
 level = st.selectbox(
     'Project Level',
